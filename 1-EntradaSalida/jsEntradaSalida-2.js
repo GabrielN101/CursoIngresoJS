09a17f4f0678232f4ba48f92abd4aaa53b8b8934
Gabriel Pìriz <36174305+GabrielN101@users.noreply.github.com>
2018-02-05 20:54:40 -0300

    ejercicio 1 y2
    ver el alert. prompt, variables y concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,8 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var person = prompt("Ingrese nombre");//Aca se ingresa nombre
+	alert("Su nombre es: " +person); //Se muestra el sujeto
+
 }
 
