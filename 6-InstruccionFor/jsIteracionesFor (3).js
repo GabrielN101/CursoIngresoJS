b69023f60e718a13a24b3a9e725618420f7b7a03
Gabriel Pìriz <36174305+GabrielN101@users.noreply.github.com>
2018-02-20 22:11:04 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,15 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
+var contador= 0;
+
+repeticiones=parseInt(repeticiones);
+
+for(contador<repeticiones;contador==repeticiones;contador++)
+{
+	alert("Hola UTN FRA");
+}
 
 
 
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,25 @@
 function Mostrar()
 {
+	var numero= prompt("Ingrese Numero: ");
+	var contadorDiv=0;
+	numero=parseInt(numero);
 
+	for (anterior=2;anterior<numero;anterior++)
+	{
+		if(numero%anterior==0)
+		{
+			document.write("<br>"+anterior);
+			contadorDiv++;
+		}
+		if(anterior>(numero/2))
+		{
+			break;
+		}
+	}
+if(contadorDiv==0)
+{
+	document.write("Es primo");
+}
 
 
 
