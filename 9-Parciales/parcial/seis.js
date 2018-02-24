4a81222b47540505cd98e4634e9fe590d2396e3c
Gabriel Pìriz <36174305+GabrielN101@users.noreply.github.com>
2018-02-24 09:34:08 -0300

    Add files via upload

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,41 @@
 function Mostrar()
 {
+	var numero;
+	var contador=0;
+	var maximo = 0;
+	var minimo = 0;
 
+	while(contador<24)
+	{
+		contador++;
+
+		numero= prompt("Ingrese Numero: ");
+
+			while(numero<=0)
+			{
+				numero= prompt("Ingrese Numero mayor a 0: ");
+				numero= parseInt(numero);
+			}
+
+	
+		
+		if(contador==1)
+		{
+			maximo=numero;
+			minimo=numero;
+		}
+		else
+		{
+			if(numero>maximo)
+			{
+				maximo=numero;
+			}
+			if(numero<minimo)
+			{
+				minimo=numero;
+			}
+		}
+	}
+	alert("El importe maximo fue: " +maximo);
+	alert("El importe minimo fue: " +minimo);
 }
