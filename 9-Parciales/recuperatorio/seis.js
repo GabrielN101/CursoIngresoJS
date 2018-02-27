16997c5bf5bf699cace04e9bd2a10413740be7e1
Gabriel Pìriz <36174305+GabrielN101@users.noreply.github.com>
2018-02-26 22:18:17 -0300

    Add files via upload

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,37 @@
 function Mostrar()
 {
+	var peso;
+	var maximo;
+	var minimo;
 
+	for(contenedor=0;contenedor<10;contenedor++)
+	{
+		peso = prompt("Ingrese peso del contenedor: "+contenedor);
+
+		peso = parseInt(peso);
+
+		while(isNaN(peso)||peso<=0)
+		{
+			peso = prompt("Ingrese un peso valido: ");
+			peso = parseInt(peso);
+		}
+
+		if(contenedor==0)
+		{
+			maximo=peso;
+			minimo=peso;
+		}
+		else
+		{
+			if(peso>maximo)
+			{
+				maximo=peso;
+			}
+			if(peso<minimo)
+			{
+				minimo=peso;
+			}
+		}
+	}
+	alert("El mayor contenedor pesa: "+maximo+" y el menor pesa: "+minimo);
 }
