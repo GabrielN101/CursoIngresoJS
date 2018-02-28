8f06b32b9cfd08ac8236def793a08ae31602a3f9
Gabriel Pìriz <36174305+GabrielN101@users.noreply.github.com>
2018-02-27 22:11:54 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,56 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 
+ 	var edad;
+ 	var sexo;
+ 	var estadoCivil;
+ 	var sueldo;
+ 	var legajo;
+ 	var nacionalidad;
+
+ 	edad = prompt("Ingrese edad: ");
+ 	while(isNaN(edad)||edad<18||edad>90)
+ 	{
+ 		edad = prompt("Ingrese una edad valida: ");
+ 	}
+
+ 	sexo = prompt("Ingrese sexo; M-Maculino, F-Femenino: ");
+ 	while(sexo!="m"&&sexo!="f")
+ 	{
+ 		sexo = prompt("Ingrese un sexo valido: ");
+ 	}
+
+ 	estadoCivil = prompt("Ingrese Estado Civil; 1-Soltero, 2-Casado, 3-Divorciado, 4-Viudo");
+ 	estadoCivil = parseInt(estadoCivil);
+
+ 	while(estadoCivil<1||estadoCivil>4||isNaN(estadoCivil))
+ 	{
+ 		estadoCivil = prompt("Ingrese un Estado Civil valido: ");
+ 	}
+
+ 	sueldo = prompt("Ingrese sueldo bruto: ");
+ 	while(isNaN(sueldo)||sueldo<8000)
+ 	{
+ 		sueldo = prompt("Ingrese sueldo bruto valido: ");
+ 	}
+
+
+ 	legajo = prompt("Ingrese su numero de legajo: ");
+ 	while(isNaN(legajo)||legajo<1000||legajo>9999)
+ 	{
+ 		legajo = prompt("Ingrese un numero de legajo valido: ");
+ 	}
+
+ 	nacionalidad = prompt("Ingrese nacionalidad; A-Argentino, E-Extranjero, N-Nacionalizado ");
+ 	while(nacionalidad!="a"&&nacionalidad!="e"&&nacionalidad!="n")
+ 	{
+ 		nacionalidad = prompt("Vuelva a ingresar una nacionalidad valida; A-Argentino, E-Extranjero, N-Nacionalizado ");
+ 	}
+
+ 	document.getElementById('Edad').value = edad;
+ 	document.getElementById('Sexo').value = sexo;
+ 	document.getElementById('EstadoCivil').value = estadoCivil;
+ 	document.getElementById('Sueldo').value = sueldo;
+ 	document.getElementById('Legajo').value = legajo;
+ 	document.getElementById('Nacionalidad').value = nacionalidad;
 }
